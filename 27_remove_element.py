3ac6c258106348796f11bb200a26b22502781880
My Bui <my.bui.fi@gmail.com>
2023-08-15 10:04:52 +0300

    add solution 2 27 remove element

--- 27_remove_element.py
+++ 27_remove_element.py
@@ -1,6 +1,11 @@
 # https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
 
-class Solution:
+# notes:
+## order doesn't matter
+## put all elements not equal to val at the beginning of the array
+## after that, the elements don't matter even though there are some equal to val
+
+class Solution1:
     def removeElement(self, nums: List[int], val: int) -> int:
         i = 0
         k = 0
@@ -9,7 +14,6 @@ class Solution:
                 nums[k] = nums[i]
                 k += 1
             i += 1
-
         return k
 
         # 1st: [0*,1,2,2,3,0,4,2] i = 1, k = 1
@@ -20,3 +24,12 @@ class Solution:
         # 6th: [0,1,3,2,3,0*,4,2] i = 6, k = 3
         # 7th: [0,1,3,0,3,0,4*,2] i = 7, k = 4
         # 8th: [0,1,3,0,4,0,4,2*] i = 8, k = 5
+
+class Solution2:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
